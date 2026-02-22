8abf2d556df1018111b7f7a36996f7b06e83aefe
Jay Haslam <jakerslam@gmail.com>
2026-02-21 19:56:09 -0700

    refactor(ts): harden security secret and egress wrappers

--- systems/security/egress_gateway.ts
+++ systems/security/egress_gateway.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// @ts-nocheck
 'use strict';
 
 const {
@@ -18,7 +17,7 @@ function usage() {
 }
 
 function parseArgs(argv) {
-  const out = { _: [] };
+  const out = { _: [] } as Record<string, any>;
   for (const arg of argv) {
     if (!arg.startsWith('--')) {
       out._.push(arg);
@@ -90,3 +89,4 @@ function main() {
 if (require.main === module) {
   main();
 }
+export {};
