9fd1304a516b14e39a625fc681f303e4082c3c36
Jay Haslam <jakerslam@gmail.com>
2026-03-04 01:57:08 -0700

    feat(rust50): migrate directive_tier_reservation_need primitive to Rust with bridge + parity tests

--- systems/autonomy/backlog_autoscale_rust_bridge.ts
+++ systems/autonomy/backlog_autoscale_rust_bridge.ts
@@ -165,6 +165,7 @@ function runBacklogAutoscalePrimitive(mode: string, data: AnyObj = {}, opts: Any
     directive_tier_weight: 'directive_tier_weight_input',
     directive_tier_min_share: 'directive_tier_min_share_input',
     directive_tier_coverage_bonus: 'directive_tier_coverage_bonus_input',
+    directive_tier_reservation_need: 'directive_tier_reservation_need_input',
     execution_reserve_snapshot: 'execution_reserve_snapshot_input',
     qos_lane_share_cap_exceeded: 'qos_lane_share_cap_exceeded_input',
     qos_lane_from_candidate: 'qos_lane_from_candidate_input',
