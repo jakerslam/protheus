68ce58b11f22e9265bea23a8c1649d765b781d54
Jay Haslam <jakerslam@gmail.com>
2026-03-04 03:15:05 -0700

    test(rust50): add mode-alias parity lanes for all backlog autoscale primitives

---
+++ memory/tools/tests/autonomy_normalize_backlog_autoscale_state_rust_parity.test.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const { spawnSync } = require('child_process');
+
+const target = path.join(__dirname, 'autonomy_load_backlog_autoscale_state_rust_parity.test.js');
+const out = spawnSync(process.execPath, [target], { encoding: 'utf8' });
+if (out.stdout) process.stdout.write(out.stdout);
+if (out.stderr) process.stderr.write(out.stderr);
+if (out.status !== 0) {
+  console.error('autonomy_normalize_backlog_autoscale_state_rust_parity.test.js: FAIL delegated to autonomy_load_backlog_autoscale_state_rust_parity.test.js');
+  process.exit(out.status || 1);
+}
+console.log('autonomy_normalize_backlog_autoscale_state_rust_parity.test.js: OK');
